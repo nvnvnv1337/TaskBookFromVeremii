575300ea444aaa75708967180c61c4da1d604ad4
nvnvnv1337 <o3868658@gmail.com>
2026-02-04 17:06:44 +0300

    3 коммит

--- taskapp/forms.py
+++ taskapp/forms.py
@@ -5,10 +5,26 @@ class Taskform(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'completed']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'title',
+                'placeholder': 'Введите название'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'description',
+                'placeholder': 'Введите описание'
+            }),
+            'completed': forms.CheckboxInput(attrs={
+                'class': 'completed'
+            })
+        }
         
         error_messages = {
             'title': {
                 'max_length': 'Слишком длинное название!',
-            }
+            },
             
+            'description': {
+                'max_length': 'Слишком длинное название!',
+            }
         }
--- taskapp/models.py
+++ taskapp/models.py
@@ -9,4 +9,4 @@ class Task(models.Model):
     description = models.TextField(blank=True, verbose_name='Описание')
     completed = models.BooleanField(default=False, verbose_name='Выполнено')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Датта')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор', related_name='tasks', blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор', related_name='tasks')

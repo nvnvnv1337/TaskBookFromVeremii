a77e1710a0d37e260840e5cd25d0b33a687bff05
nvnvnv1337 <o3868658@gmail.com>
2026-04-01 15:09:26 +0300

    мини фиксы

--- tasks/settings.py
+++ tasks/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_TOKEN')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.119', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['192.168.1.119', '127.0.0.1', 'localhost',]
 
 # Application definition
 
